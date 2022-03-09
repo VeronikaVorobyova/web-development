f08d705a61f1bcfaed54e2eca7e14d308de577bd
Vov4ik3587 <galstyan.vova@gmail.com>
2022-03-09 21:34:36 +0700

    структуризация:добавление шаблонов, данных для авторизации

---
+++ data/database.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Хранение данных для подключения к БД
+ */
+
+$database = [
+    'host'=> 'localhost',
+    'db_name'=>'mysite',
+    'user' => 'root',
+    'password'=>''
+];
---
+++ utils/connect.php
@@ -0,0 +1,8 @@
+<?php
+
+$database =require_once ('./data/database.php');
+$connection = @mysqli_connect($database['host'], $database['user'],
+    $database['password'], $database['db_name']);
+$query = 'SET NAMES utf8';
+$result = @mysqli_query($connection, $query);
+?>
