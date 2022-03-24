229485d5756e2e0b42c7569c6b37558adbb9598a
Vov4ik3587 <galstyan.vova@gmail.com>
2022-03-24 14:49:18 +0700

    хм....

--- index.php
+++ index.php
@@ -1,27 +1,9 @@
 <?php
-/*// Начинаем сессию и подключаемся к БД
+// Начинаем сессию и подключаемся к БД
 session_start();
 require_once 'utils/connect.php';
 // ------------------------------------------
 // Отрисовка
 require_once 'templates/header.php';
 require_once 'templates/mainPage.php';
-require_once 'templates/footer.php';*/
-
-$database = [
-    'host'=> 'localhost',
-    'db_name'=>'mysite',
-    'user' => 'root',
-    'password'=>''
-];
-
-$link = new mysqli($database['host'], $database['user'],
-    $database['password'], $database['db_name']);
-
-/**
- * SQL-запрос новости, которая далее будет помещаться в pageNew и blockNews
- */
-$query = 'SELECT COUNT(*) as count FROM news';
-$count = mysqli_query($link, $query);
-//$res = $count->fetch_assoc();
-var_dump($count);
+require_once 'templates/footer.php';
