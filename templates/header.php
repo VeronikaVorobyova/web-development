5e7bae91e12ba2fbfbb832e699f2131becbc5da5
Vov4ik3587 <galstyan.vova@gmail.com>
2022-04-22 00:01:46 +0700

    добавил кодировку UTF-8

--- templates/header.php
+++ templates/header.php
@@ -4,8 +4,9 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/utils/connect.php';
 ?>
 
 <head>
+    <meta charset="UTF-8">
     <link rel="stylesheet" href="/templates/style/header.css">
-    <link rel=" stylesheet" href="/templates/style/footer.css">
+    <link rel="stylesheet" href="/templates/style/footer.css">
     <link rel="stylesheet" href="/templates/style/mainPage.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Новостной сайт</title>
